8678dd8ce513ca2e84c4c0aa5cbd341fbc80fe34
Eugene <leugenel@gmail.com>
2021-10-25 14:02:59 +0300

    Add files via upload
    Fixed issue with the same digits

--- bagles.py
+++ bagles.py
@@ -1,14 +1,16 @@
 from random import shuffle
 from unittest.mock import Mock, patch, PropertyMock, MagicMock
 from unittest import TestCase
-
+from argparse import ArgumentParser
 
 class Bagles:
 
-    GUESSES = 1
+    GUESSES = 10
     NUMBER_OF_DIGITS = 3
 
-    def __init__(self) -> None:
+    def __init__(self, n_guesses: int = GUESSES, silent : bool = False) -> None:
+        self.silent = silent
+        self.n_guesses = n_guesses
         first_digits = [i for i in range(1,10)]
         shuffle(first_digits)
         self.__hidden_number = "".join(str(c) for c in first_digits[:self.NUMBER_OF_DIGITS])
@@ -18,7 +20,48 @@ class Bagles:
         return self.__hidden_number
 
     def bagles(self):
-        print("""
+        
+        print(self.hidden_number) if not self.silent else None
+
+        for i in range(self.n_guesses):
+            user_input = self.get_user_input(i)
+            if not user_input:
+                continue
+            if user_input == self.hidden_number:
+                print("Great you won! This is a number!") if not self.silent else None
+                return
+            bagles = True
+            for i in range(self.NUMBER_OF_DIGITS):
+                if user_input[i] == self.hidden_number[i]:
+                    print("Fermi") if not self.silent else None
+                    bagles = False
+                    continue
+                if user_input[i] in self.hidden_number:
+                    print("Pico") if not self.silent else None
+                    bagles = False  
+                    continue
+            if bagles:
+                print("Bagles")
+
+    def get_user_input(self, counter: int = 0) -> str:
+        user_input = input(f"Guess # {counter} ==>:")
+        try:
+            if len(list(set(user_input))) != self.NUMBER_OF_DIGITS:
+                print("Please provide different digits in the number")
+                raise ValueError
+        except ValueError:
+            print("You should enter 3-digit number with the different digits")
+            return None
+        return user_input
+
+class BaglesTest(TestCase):
+    @patch('builtins.input', return_value='123')
+    def test_bagles(self, input):
+        bgl = Bagles()
+        type(bgl).hidden_number = PropertyMock(return_value="124")
+        bgl.bagles()
+       
+greeting = """
             I am thinking of a 3-digit number. Try to guess what it is.
             Here are some clues:
             When I say:
@@ -30,63 +73,19 @@ class Bagles:
             No digit is correct.
             Bagels
             I have thought up a number.
-            You have 10 guesses to get it.""")
+            All digits should be different.
+            You have 10 guesses to get it."""
 
-        print(self.hidden_number)
-
-        for i in range(self.GUESSES):
-            user_input = self.get_user_input(i)
-            if not user_input:
-                continue
-            if user_input == self.hidden_number:
-                print("Great you won! This is a number!")
-                return
-            bagles = True
-            for i in range(self.NUMBER_OF_DIGITS):
-                if user_input[i] == self.hidden_number[i]:
-                    print("Fermi")
-                    bagles = False
-                    continue
-                if user_input[i] in self.hidden_number:
-                    print("Pico")
-                    bagles = False  
-                    continue
-            if bagles:
-                print("Bagles")
-
-    @staticmethod
-    def get_user_input(counter: int = 0) -> str:
-        user_input = input(f"Guess # {counter} ==>:")
-        try:
-            if 999 < int(user_input) or int(user_input) < 99:
-                raise ValueError
-        except ValueError:
-            print("You should enter 3-digit number")
-            return None
-        return user_input
-
-class BaglesTest(TestCase):
-    @patch('builtins.input', return_value='123')
-    def test_bagles(self, input):
-        # with patch('bagles.Bagles.hidden_number') as bmock:
-        #     # instance = bmock.return_value
-        #     # instance.number = "124"
-        #     #bmock.return_value = "124"
-        #     type(bmock.return_value).hidden_number = PropertyMock(return_value='123')
-        #     binstance = Bagles
-        #     print(bmock.hidden_number)
-        #     print(binstance.hidden_number)
-        #     print("end")
-        bgl = Bagles()
-        type(bgl).hidden_number = PropertyMock(return_value="124")
-        bgl.bagles()
-        # print(bgl.hidden_number)
-        # assert bgl.hidden_number == "124", f"{bgl.hidden_number}"
-        # Bagles = Mock()
-        # Bagles.hidden_number.return_value = "124"
-        # Bagles().bagles()
- 
+def parse():
+    parser = ArgumentParser()
+    parser.add_argument('-n', dest='num_guesses', type=int, default=10, required=False)
+    parser.add_argument('-s', dest='silent',  action='store_true', default=False)
+    parser.add_argument('-t', dest='test', action='store_true', default=False)
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    #game = Bagles().bagles()
-    BaglesTest().test_bagles()
+    args = parse()
+    if not args.silent:
+        print(greeting)
+    Bagles(n_guesses=args.num_guesses, silent=args.silent).bagles() if not args.test else BaglesTest().test_bagles()
+   

c318395d39d3265304159c045f1b6faf91d12bef
Eugene <leugenel@gmail.com>
2021-11-03 12:20:05 +0200

    Add files via upload
    Adding thread for machine guess

--- bagles.py
+++ bagles.py
@@ -2,18 +2,19 @@ from random import shuffle
 from unittest.mock import Mock, patch, PropertyMock, MagicMock
 from unittest import TestCase
 from argparse import ArgumentParser
+from threading import Thread
 
 class Bagles:
 
     GUESSES = 10
     NUMBER_OF_DIGITS = 3
+    machine_guess_number = "123"
 
-    def __init__(self, n_guesses: int = GUESSES, silent : bool = False) -> None:
+    def __init__(self, n_guesses: int = GUESSES, silent : bool = False, machine_input: bool = False) -> None:
         self.silent = silent
         self.n_guesses = n_guesses
-        first_digits = [i for i in range(1,10)]
-        shuffle(first_digits)
-        self.__hidden_number = "".join(str(c) for c in first_digits[:self.NUMBER_OF_DIGITS])
+        self.machine_input=machine_input
+        self.__hidden_number = self.coin_number()
     
     @property
     def hidden_number(self):
@@ -35,6 +36,10 @@ class Bagles:
         print(f"The number was {self.hidden_number}") if not self.silent else None
         return False
     
+    def machine_guess(self):
+        self.machine_guess_number = self.coin_number()
+        print(f"Machine guess: {self.machine_guess_number}")
+
     def print_round_res(self, res: dict):
         if res.get("Bagles", False):
             print("Bagles")
@@ -59,14 +64,25 @@ class Bagles:
         return res
 
     def get_user_input(self, counter: int = 0) -> str:
+        if self.machine_input:
+            machine_thread = Thread(target=self.machine_guess)
+            machine_thread.start()
+            machine_thread.join()
+            return self.machine_guess_number
+            
         user_input = input(f"Guess # {counter} ==>:")
         try:
             if 999 < int(user_input) or int(user_input) < 100 or len(list(set(user_input))) != self.NUMBER_OF_DIGITS:
                 raise ValueError
         except ValueError:
-            print("You should enter 3-digit number with the different digits")
+            print(f"You should enter {Bagles.NUMBER_OF_DIGITS}-digit number with the different digits")
             return None
         return user_input
+
+    def coin_number(self):
+        first_digits = [i for i in range(1,10)]
+        shuffle(first_digits)
+        return "".join(str(c) for c in first_digits[:self.NUMBER_OF_DIGITS])
 
 # In order to run unittets run: python -m unittest bagles.py 
 class BaglesTest(TestCase):
@@ -120,7 +136,7 @@ class BaglesTest(TestCase):
         assert res["Bagles"] == False       
 
 greeting = """
-            I am thinking of a 3-digit number. Try to guess what it is.
+            I am thinking of a {}-digit number. Try to guess what it is.
             Here are some clues:
             When I say:
             Pico
@@ -136,14 +152,14 @@ greeting = """
 
 def parse():
     parser = ArgumentParser()
-    parser.add_argument('-n', dest='num_guesses', type=int, default=10, required=False)
-    parser.add_argument('-s', dest='silent',  action='store_true', default=False)
+    parser.add_argument('-n', dest='num_guesses', type=int, default=Bagles.GUESSES, required=False)
+    parser.add_argument('-s', dest='silent',  action='store_true', default=False, required=False)
+    parser.add_argument('-m', dest='machine_input',  action='store_true', default=False, required=False)
     return parser.parse_args()
-
 
 if __name__ == '__main__':
     args = parse()
     if not args.silent:
-        print(greeting)
-    Bagles(n_guesses=args.num_guesses, silent=args.silent).bagles()
+        print(greeting.format(Bagles.NUMBER_OF_DIGITS))
+    Bagles(n_guesses=args.num_guesses, silent=args.silent, machine_input=args.machine_input).bagles()
    
